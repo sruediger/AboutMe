16c124ac6afe0fbd67e5f75ef0e2138cd8109d07
Sérgio Ruediger <sruediger@protonmail.com>
2022-04-26 20:28:41 -0300

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Sérgio Ruediger on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by Sérgio Ruediger on 26/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
