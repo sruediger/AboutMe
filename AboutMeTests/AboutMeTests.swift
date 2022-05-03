9f8009b7f0d05c5d3a87f4306ae7e5bc45472380
Sérgio Ruediger <sruediger@protonmail.com>
2022-05-03 15:49:01 -0300

    Updated backend unit tests

--- AboutMeTests/AboutMeTests.swift
+++ AboutMeTests/AboutMeTests.swift
@@ -8,15 +8,44 @@
 import XCTest
 @testable import AboutMe
 
+extension AboutMeTests {
+    
+    /// Test if the assets used by the app are in it
+    internal func testLoadAssets() {
+        print("Trying to load all AboutMeApp assets...")
+        if let assets = self.dummy?.assets {
+            assets.forEach { XCTAssertNotNil($0) }
+        }else { XCTAssertNil(nil); }
+    }
+    
+    /// Test if the URLs used by the app are valid
+    internal func testLoadURLs() {
+        print("Trying to load all Sergio's contacts & apps URLs...")
+        if let urls = self.dummy?.URLs {
+            urls.forEach { XCTAssertNotNil($0) }
+        }else { XCTAssertNil(nil); }
+    }
+    
+}
+
 class AboutMeTests: XCTestCase {
-
-    override func setUpWithError() throws {
+    private var dummy: TestDummy? = nil
+    
+    override func setUp() {
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.dummy = TestDummy()
     }
-
-    override func tearDownWithError() throws {
+    
+    override func tearDown()  {
+        super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.dummy = nil
     }
+    
+    override func setUpWithError() throws { }
+
+    override func tearDownWithError() throws { }
 
     func testExample() throws {
         // This is an example of a functional test case.
