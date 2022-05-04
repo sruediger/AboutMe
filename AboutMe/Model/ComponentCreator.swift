2d2a2f2dc5bf8fa74579009d7f67a0020ac28752
Sérgio Ruediger <sruediger@protonmail.com>
2022-05-04 10:26:28 -0300

    Added ComponentCreator

---
+++ AboutMe/Model/ComponentCreator.swift
@@ -0,0 +1,15 @@
+//
+//  ComponentCreator.swift
+//  AboutMe
+//
+//  Created by Sérgio Ruediger on 04/05/22.
+//
+
+import Foundation
+
+/// Structure that conforms to the ComponentFactory protocol to inherit it's methods and create the views
+struct ComponentCreator: ComponentFactory {
+    // Reimplement the ComponentFactory protocol methods if necessary
+
+    
+}
