b66ce8c4bd8ccad46d19b085054e6affb0a2ca05
Sérgio Ruediger <sruediger@protonmail.com>
2022-05-04 11:05:13 -0300

    Removed unused methods that were moved to it's own viewModel

--- AboutMe/ViewModel/ModalViewModel.swift
+++ AboutMe/ViewModel/ModalViewModel.swift
@@ -29,34 +29,13 @@ extension ModalViewModel {
     
     internal mutating func tapped(webItem: WebViewRepresentable) {
         self.webView.contentURL = webItem.url
-        if webItem.url.absoluteString.contains("itms-apps") { // Open app Store
+        if webItem.url.absoluteString.contains("itms-apps") { // Open app Store (unavailable on simulator)
             UIApplication.shared.open(webItem.url, options: [:], completionHandler: nil)
         }else { // Open Safari Preview
             self.webView.show = true
         }
     }
-    
-    internal func getSkillsTitle(of index: Int) -> String {
-        switch index {
-            case 0: return "iOS Development"
-            case 1: return "Technical Skills"
-            case 2: return "Databases"
-            case 3: return "Design"
-            case 4: return "Soft Skills"
-            default: return ""
-        }
-    }
-    
-    internal func getSkillsItems(of index: Int) -> [AnimatableScroll] {
-        switch index {
-            case 0: return TechSkills.iOSDevelopment
-            case 1: return TechSkills.generalDevelopment
-            case 2: return TechSkills.databases
-            case 3: return TechSkills.design
-            default: return []
-        }
-    }
-    
+
     internal func getSizeOf(content: MainScreenSubview?) -> CGSize {
         guard let subview = content else { return .zero }
         let deviceWidth = UIScreen.main.bounds.width
