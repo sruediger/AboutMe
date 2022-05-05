645bd9ba65146a04767417edbab43cd2520e2e1a
Sérgio Ruediger <sruediger@protonmail.com>
2022-05-05 12:03:51 -0300

    Removed default unused testLaunchPerformance method

--- UnitTests/AboutMeUITests/MainScreenUITests.swift
+++ UnitTests/AboutMeUITests/MainScreenUITests.swift
@@ -63,13 +63,4 @@ final class MainScreenUITests: XCTestCase {
         NSLog("%@", "tearDownWithError")
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
