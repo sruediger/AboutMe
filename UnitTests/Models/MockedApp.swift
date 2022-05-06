f8781a63bad27de98cf1e37dcd7321b15c6b9bfb
Sérgio Ruediger <sruediger@protonmail.com>
2022-05-06 00:35:47 -0300

    Added localizable strings support

--- UnitTests/Models/MockedApp.swift
+++ UnitTests/Models/MockedApp.swift
@@ -15,7 +15,15 @@ public final class MockedApp {
     
     // - MARK: TODO move all app strings to Localizable file and create models to turn them and other components accessible to the main and both tests targets
     
-    internal lazy var mainScreenTexts: [String] = ["About Me", "Sérgio Gomes Ruediger", "iOS developer & software engineer", "Brasília, Brazil", "Languages", "Skills", "Apps", "Experience", "Curiosities", "Contact"]
+    internal lazy var mainScreenTexts: [String] = [String.localized("STXMainTitle"), "Sérgio Gomes Ruediger", String.localized("STXOccupation"), String.localized("STXLocation"), String.localized("BTNLanguages"), String.localized("BTNSkills"), String.localized("BTNApps"), String.localized("BTNExperiences"), String.localized("BTNCuriosities"), String.localized("BTNContact")]
+    
+    /// Helper method that fetch an NSLocalizedString. By default the XCTest framework doesn't enable string localization, that's why an auxiliary method is required besides the String+Utils localized method.
+    /// - Parameter Identifier: Localized String identifier, defined at Localizable.strings
+    /// - Class that called this method
+    /// - Returns the localized String for the specific caller
+    public func localizedString(_ identifier: String, _ caller: AnyClass) -> String {
+        return NSLocalizedString(identifier, bundle: Bundle(for: caller), comment: "")
+    }
     
     /// Assert that a button exists and tap it
     /// - Parameter button: XCUIElement object that represent this app's buttons
@@ -39,7 +47,6 @@ public final class MockedApp {
             case .landscapeRight: device.orientation = .portrait
             default: device.orientation = .portrait
         }
-        
     }
     
 }
